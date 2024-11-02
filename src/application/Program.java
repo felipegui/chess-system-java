4edd8f5e9bd692900a426c2d115544baa47186ff
Felipe Guimaraes <fguiengenharia@gmail.com>
2024-11-02 20:04:00 -0300

    Special Move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,4 +1,4 @@
-epackage application;
+package application;
 
 import java.util.ArrayList;
 import java.util.InputMismatchException;
@@ -37,6 +37,12 @@ public class Program {
 				
 				if(capturedPiece != null) {
 					captured.add(capturedPiece);
+				}
+				if (chessMatch.getPromoted() != null) {
+					System.out.print("Enter piece for promotion (B / N / R / Q): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+
 				}
 			}
 			catch (ChessException e) {
